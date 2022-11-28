b04c9fb58fd818cec95d49da70520da6b85cf4ad
antonio-mani <73863212+antonio-mani@users.noreply.github.com>
2022-11-27 18:38:19 -0800

    Pagerank done

--- app.py
+++ app.py
@@ -16,41 +16,63 @@ import whoosh
 from whoosh.index import create_in
 from whoosh.index import open_dir
 from whoosh.fields import *
+from whoosh.qparser import QueryParser
 from whoosh.qparser import MultifieldParser
+from whoosh import qparser
+from whoosh import scoring
 
+import pickle
 
-
+ 
 app = Flask(__name__)
+
+# Open our dictionary containing page rank values
+with open('./src/pickles/pageRank.pickle','rb') as handle:
+    myDict = pickle.load(handle)
+
+# Open category list
+with open("./src/pickles/categoryList.pickle", "rb") as handle:
+    categories = pickle.load(handle)
+
+# Render Home Page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     print("Someone is at the home page.")
-    return render_template('welcome_page.html')
+    return render_template('welcome_page.html', categories=categories)
 
 @app.route('/my-link/')
 def my_link():
     print('I got clicked!')
     return 'Click.' 
 
+# Load results page
 @app.route('/results/', methods=['GET', 'POST'])
 def results():
+
     global mySearcher
     if request.method == 'POST':
         data = request.form
     else:
         data = request.args
 
+    # Grab our query and category if selected
     query = data.get('test')
-    test = data.get('test')
+    cat = data.get('category')
+
+    # Create searcher and search
     mySearcher = MyWhooshSearcher()
-    titles, descriptions = mySearcher.search(query)
+    titles, descriptions = mySearcher.search(query, cat)
+
 
     urls = []
     contents = []
+
+    # Grab descriptions and external link
     for i, description in enumerate(descriptions):
         temp = description.split('\n', 1)
         urls.append(temp[0][6:])
         description = temp[1]
-        print(description)
+        #print(description)
 
         current_desc = ''
         for word in query.split():
@@ -62,12 +84,12 @@ def results():
             current_desc = " ".join(description[:20])
         contents.append(current_desc)
 
+        #url = contents.split('\n')
 
-    print("You searched for: " + query)
-    print("Alternatively, the second box has: " + test)
+    # print("You searched for: " + query)
+    # print("Alternatively, the second box has: " + test)
  
-    return render_template('results.html', query=query, results=zip(urls, contents))
-
+    return render_template('results.html', query=query, results=zip(urls,contents), categories=categories)
 
 def find_substring(base, term):
     base = base.split()
@@ -76,35 +98,86 @@ def find_substring(base, term):
             return base[i-10:i+10]
     return 0
 
+# ******* NO LONGER IN USE. WILL REMOVE ON FINAL REFACTOR ******
+@app.route('/resultList', methods=['GET', 'POST'])
+def resultList():
+    if request.method == 'POST':
+        data = request.form 
+    else:
+        data = request.args 
+    string = data.get('type')
+    #print(f'String is: {string}')
+    images=''
+    with open(f'../HW4/pagesMe/{string}', 'r') as f:
+        for line in f:
+            if line.startswith("Images:"):
+                images = line[9:len(line)-2].strip("'").split(',')
+                for img in range(len(images)):
+                    try:
+                        images[img] = images[img][:images[img].index(".png") + 4]
+                    except:
+                        try:
+                            images[img] = images[img][:images[img].index(".jpg") + 4]
+                        except:
+                            continue
+                      
+                
+    with open(f'../HW4/pagesMe/{string}', 'r') as f:
+        text = f.read()
+    return render_template('content.html', text=text, images=images)
+
+def loadImages(f):
+    with open(f'../HW4/pagesMe/{string}', 'r') as f:
+        for line in f:
+            if line.startswith("Images:"):
+                images = line[9:len(line)-2].strip("'").split(',')
+                for img in range(len(images)):
+                    images[img] = images[img][:images[img].index(".png") + 4]
+                return(images)
+    return(images)
+
+# ******** END OF REMOVE BLOCK *******
+
+# Whoosh searcher using page rank
 class MyWhooshSearcher(object):
     """docstring for MyWhooshSearcher"""
     def __init__(self):
         #self.indexer = index.open_dir('myIndex')
         super(MyWhooshSearcher, self).__init__()
-
-
-
-        
-    def search(self, queryEntered):
+         
+    # Need to add var for keyword search for andvanced search ***************   
+    def search(self, queryEntered, category):
         title = list()
         description = list()
         
         # Created our own indexer for now. Could init in constructor later
         indexer = whoosh.index.open_dir("myIndex")
-
-
         with indexer.searcher() as search:
             query = MultifieldParser(['title', 'textdata'], schema=indexer.schema)
-            # query = query.parse(queryEntered)
             query = query.parse(queryEntered)
 
             # Set limit to 10 will need to change later as we display 10 per page not 10 total
-            results = search.search(query, limit=10)
-            
-            # Getting list of titles and page content
-            for x in results:
-                title.append(x['title'])
-                description.append(x['textdata'])
+            results = search.search(query, limit=None, terms=True)
+            newDict = {}
+            otherDict = {} 
+
+            # Iterate through results to match titles 
+            for i in results:
+                if i['title'] in myDict:
+                    #newDict[i['title']] = myDict[i['title']]
+                    #print(f"{i['title']} {myDict[i['title']]}")
+                    otherDict[i['title']] = myDict[i['title']]
+             
+            sortedDict = dict(sorted(otherDict.items(), key=lambda x:float(x[1]), reverse=True))
+
+            # Now dict is sorted add raw text as values
+            for i in results:
+                sortedDict[i['title']] = i['textdata']
+
+            # Make list from keys and values and return them
+            title = list(sortedDict.keys())
+            description = list(sortedDict.values())
+           # print(f'\nTITLES AFTER: {title}\n')
             
         return title, description
 
